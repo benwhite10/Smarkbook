0a1e5f0fd26078387d3ff09232b271035b325598
benwhite10 <ben.white10@outlook.com>
2015-11-10 21:20:05 +0000

    Add forgotten password method

--- public_html/includes/mail_functions.php
+++ public_html/includes/mail_functions.php
@@ -6,37 +6,33 @@ if(isset($_POST['email'], $_POST['name'])){
     send_mail(CONTACT, $_POST['email'], $_POST['name'], $_POST['body']);
 }
 
-function send_mail($account, $to, $name, $body){
-	/*if($account == CONTACT){
-		$account = 'contact.smarkbook@gmail.com';
-	}else{
-		$account = 'contact.smarkbook@gmail.com';
-	}*/
-	
-	$mail = new PHPMailer();
+function sendMailFromContact($to, $name, $body, $subject){
 
-	$mail->IsSMTP();  // telling the class to use SMTP
-	$mail->SMTPAuth   = true; // SMTP authentication
-	$mail->Host       = "smtp.gmail.com"; // SMTP server
-	$mail->Port       = 465; // SMTP Port
-	$mail->Username   = "contact.smarkbook@gmail.com"; // SMTP account username
-	$mail->Password   = "Jedwards1";        // SMTP account password
-	$mail->SMTPSecure = 'ssl';
+    $mail = new PHPMailer();
 
-	$mail->SetFrom($account, 'Smarkbook'); // FROM
-	$mail->AddReplyTo($account, 'Smarkbook'); // Reply TO
+    $mail->IsSMTP();  // telling the class to use SMTP
+    $mail->SMTPAuth   = true; // SMTP authentication
+    $mail->Host       = "smtp.gmail.com"; // SMTP server
+    $mail->Port       = 465; // SMTP Port
+    $mail->Username   = "contact.smarkbook@gmail.com"; // SMTP account username
+    $mail->Password   = "Jedwards1";        // SMTP account password
+    $mail->SMTPSecure = 'ssl';
 
-	$mail->AddAddress($to, $name); // recipient email
-        $mail->AddCC('ben.white10@outlook.com', 'Ben White'); //CC email
+    $mail->SetFrom("contact.smarkbook@gmail.com", 'Smarkbook'); // FROM
+    $mail->AddReplyTo("contact.smarkbook@gmail.com", 'Smarkbook'); // Reply TO
 
-	$mail->Subject    = "Welcome to Smarkbook!"; // email subject
-	$mail->Body       = $body;
+    $mail->AddAddress($to, $name); // recipient email
+    //$mail->AddCC('ben.white10@outlook.com', 'Ben White'); //CC email
 
-	if(!$mail->Send()) {
-		echo 'Message was not sent. \n';
-		echo 'Mailer error: ' . $mail->ErrorInfo;
-	} else {
-		//echo 'Message has been sent.';
-	}
+    $mail->Subject    = $subject; // email subject
+    $mail->Body       = $body;
+    $mail->IsHTML(true);
 
+    if(!$mail->Send()) {
+        echo 'Message was not sent. \n';
+        echo 'Mailer error: ' . $mail->ErrorInfo;
+    } else {
+        //echo 'Message has been sent.';
+        //infoLog("Sent")
+    }
 }
