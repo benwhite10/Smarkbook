213b8592775c00512444e37c3181029a993f6164
benwhite10 <ben.white10@outlook.com>
2016-11-27 11:30:58 +0000

    Optimise search

--- public_html/requests/searchWorksheets.php
+++ public_html/requests/searchWorksheets.php
@@ -28,19 +28,84 @@ switch ($requestType){
 }
 
 function searchWorksheets($searchTerms){
-    $query = "SELECT `Version ID` FROM `TWORKSHEETVERSION`
-            WHERE `WName` LIKE '%$searchTerms%' 
-            ORDER BY `WName`";
-    try {
-        $ids = db_select_exception($query);
-    } catch (Exception $ex) {
-        returnToPageError($ex, "There was an error running the search");
+    $searchArray = convertSearchTerms($searchTerms);
+    
+    $query = "SELECT `Version ID`, `WName` Name FROM `TWORKSHEETVERSION` WHERE ";
+    foreach($searchArray as $key => $searchTerm) {
+        if ($key != 0) $query .= " OR ";
+        $query .= "`WName` LIKE '%$searchTerm%' ";
     }
+    $query .= "ORDER BY `WName`";
+    
+    try {
+        $worksheets = db_select_exception($query);
+    } catch (Exception $ex) {
+        returnToPageError($ex, "There was an error running the search query");
+    }
+    
+    $fullSearchArray = getFullSearchArray($searchArray);
+    
+    // Score the worksheets
+    foreach($worksheets as $key => $worksheet) {
+        $worksheets[$key] = scoreWorksheet($worksheet, $fullSearchArray);
+    }
+    
+    $sorted = array_orderby($worksheets, 'Score', SORT_DESC, 'Name', SORT_ASC);
+    
     $response = array(
         "success" => TRUE,
-        "vids" => $ids);
+        "vids" => $sorted);
     echo json_encode($response);
     exit();
+}
+
+function convertSearchTerms($searchTerms) {
+    $searchArray = explode(" ", $searchTerms);
+    $finalSearchArray = array();
+    foreach($searchArray as $term) {
+        if(validateSearchTerm($term)) {
+            array_push($finalSearchArray, trim($term));
+        }
+    }
+    return $finalSearchArray;
+}
+
+function validateSearchTerm($term) {
+    $trim_term = trim($term);
+    if (strlen($trim_term) < 2) return FALSE;
+    $disallowed = array("AND", "OR", "THE", "IF", "SO", "BUT");
+    foreach($disallowed as $word) {
+        if (strcasecmp($word, $trim_term) == 0){
+            return FALSE;
+        }
+    }
+    return TRUE;
+}
+
+function getFullSearchArray($searchArray) {
+    $len = count($searchArray);
+    $fullArray = array();
+    for ($i = 0; $i < $len; $i++) {
+        for ($j = 0; $j < ($len-$i); $j++) {
+            $string = "";
+            for($k = 0; $k < ($i + 1); $k++) {
+                $string .= $searchArray[$k + $j] . " ";
+            }
+            array_push($fullArray, trim($string));
+        }
+    }
+    return $fullArray;
+}
+
+function scoreWorksheet($worksheet, $fullSearchArray) {
+    $name = $worksheet["Name"];
+    $score = 0;
+    foreach($fullSearchArray as $term) {
+        if (strpos($name, $term) !== FALSE) $score++;
+        if (stripos($name, $term) !== FALSE) $score++;
+    }
+    $worksheet["Score"] = $score;
+    return $worksheet;
 }
 
 function returnToPageError($ex, $message){
@@ -59,4 +124,21 @@ function failRequest($message){
         "message" => $message);
     echo json_encode($response);
     exit();
+}
+
+function array_orderby()
+{
+    $args = func_get_args();
+    $data = array_shift($args);
+    foreach ($args as $n => $field) {
+        if (is_string($field)) {
+            $tmp = array();
+            foreach ($data as $key => $row)
+                $tmp[$key] = $row[$field];
+            $args[$n] = $tmp;
+            }
+    }
+    $args[] = &$data;
+    call_user_func_array('array_multisort', $args);
+    return array_pop($args);
 }
