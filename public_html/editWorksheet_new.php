904a8103e6e37a15f04c5272a41ed41d9952561b
benwhite10 <ben.white10@outlook.com>
2016-12-21 12:35:15 +0000

    Add styling for tags

--- public_html/editWorksheet_new.php
+++ public_html/editWorksheet_new.php
@@ -97,6 +97,56 @@ if(!authoriseUserRoles($userRole, ["SUPER_USER", "STAFF"])){
                     <h2>Worksheet Tags</h2>
                 </div>
                 <div id="worksheet_tags" class="section_main">
+                    <div contenteditable="true" id="txtDiv" class="tags_input">
+                        <div id="tag_0_0" class="tag classification" contenteditable="false">
+                            <div id="tag_0_0_text" class="tag_text">A Level</div>
+                            <div id="tag_0_0_button" class="tag_button"></div>
+                        </div>
+                        <div id="tag_0_1" class="tag major" contenteditable="false">
+                            <div id="tag_0_1_text" class="tag_text">Differentiation</div>
+                            <div id="tag_0_1_button" class="tag_button"></div>
+                        </div>
+                        <div id="tag_0_0" class="tag minor" contenteditable="false">
+                            <div id="tag_0_1_text" class="tag_text">Product Rule</div>
+                            <div id="tag_0_1_button" class="tag_button"></div>
+                        </div>
+                        <div id="tag_0_0" class="tag classification" contenteditable="false">
+                            <div id="tag_0_0_text" class="tag_text">A Level</div>
+                            <div id="tag_0_0_button" class="tag_button"></div>
+                        </div>
+                        <div id="tag_0_1" class="tag major" contenteditable="false">
+                            <div id="tag_0_1_text" class="tag_text">Differentiation</div>
+                            <div id="tag_0_1_button" class="tag_button"></div>
+                        </div>
+                        <div id="tag_0_0" class="tag minor" contenteditable="false">
+                            <div id="tag_0_1_text" class="tag_text">Product Rule</div>
+                            <div id="tag_0_1_button" class="tag_button"></div>
+                        </div>
+                        <div id="tag_0_0" class="tag classification" contenteditable="false">
+                            <div id="tag_0_0_text" class="tag_text">A Level</div>
+                            <div id="tag_0_0_button" class="tag_button"></div>
+                        </div>
+                        <div id="tag_0_1" class="tag major" contenteditable="false">
+                            <div id="tag_0_1_text" class="tag_text">Differentiation</div>
+                            <div id="tag_0_1_button" class="tag_button"></div>
+                        </div>
+                        <div id="tag_0_0" class="tag minor" contenteditable="false">
+                            <div id="tag_0_1_text" class="tag_text">Product Rule</div>
+                            <div id="tag_0_1_button" class="tag_button"></div>
+                        </div>
+                        <div id="tag_0_0" class="tag classification" contenteditable="false">
+                            <div id="tag_0_0_text" class="tag_text">A Level</div>
+                            <div id="tag_0_0_button" class="tag_button"></div>
+                        </div>
+                        <div id="tag_0_1" class="tag major" contenteditable="false">
+                            <div id="tag_0_1_text" class="tag_text">Differentiation</div>
+                            <div id="tag_0_1_button" class="tag_button"></div>
+                        </div>
+                        <div id="tag_0_0" class="tag minor" contenteditable="false">
+                            <div id="tag_0_1_text" class="tag_text">Product Rule</div>
+                            <div id="tag_0_1_button" class="tag_button"></div>
+                        </div>
+                    </div>
                 </div>
                 <div id="worksheet_questions_title" class="section_title">
                     <h2>Questions</h2>
@@ -104,13 +154,50 @@ if(!authoriseUserRoles($userRole, ["SUPER_USER", "STAFF"])){
                 <div id="worksheet_questions" class="section_main">
                     <div id="question_1" class="worksheet_question_div">
                         <div id="question_1_details" class="worksheet_question_details">
-                            
+                            <div class="wqd_question_text">Question</div>
+                            <div contenteditable="true" class="wqd_question_input">1</div>
+                            <div contenteditable="true" class="wqd_marks_input">3</div>
+                            <div class="wqd_marks_text">Marks:</div>
                         </div>
-                        <div id="question_1_tags_summary" class="worksheet_question_tags_summary">
-                            
+                        <div id="question_1_tags_entry" class="worksheet_question_tags_entry"> 
+                            <div contenteditable="true" id="txtDiv" class="tags_input">
+                                <div id="tag_0_0" class="tag classification" contenteditable="false">
+                                    <div id="tag_0_0_text" class="tag_text">A Level</div>
+                                    <div id="tag_0_0_button" class="tag_button"></div>
+                                </div>
+                                <div id="tag_0_1" class="tag major" contenteditable="false">
+                                    <div id="tag_0_1_text" class="tag_text">Differentiation</div>
+                                    <div id="tag_0_1_button" class="tag_button"></div>
+                                </div>
+                                <div id="tag_0_0" class="tag minor" contenteditable="false">
+                                    <div id="tag_0_1_text" class="tag_text">Product Rule</div>
+                                    <div id="tag_0_1_button" class="tag_button"></div>
+                                </div>
+                            </div>
                         </div>
-                        <div id="question_1_tags_entry" class="worksheet_question_tags_entry">
-                            
+                    </div>
+                    <div id="question_2" class="worksheet_question_div">
+                        <div id="question_2_details" class="worksheet_question_details">
+                            <div class="wqd_question_text">Question</div>
+                            <div contenteditable="true" class="wqd_question_input">2</div>
+                            <div contenteditable="true" class="wqd_marks_input">7</div>
+                            <div class="wqd_marks_text">Marks:</div>
+                        </div>
+                        <div id="question_2_tags_entry" class="worksheet_question_tags_entry"> 
+                            <div contenteditable="true" id="txtDiv" class="tags_input">
+                                <div id="tag_0_0" class="tag classification" contenteditable="false">
+                                    <div id="tag_0_0_text" class="tag_text">A Level</div>
+                                    <div id="tag_0_0_button" class="tag_button"></div>
+                                </div>
+                                <div id="tag_0_1" class="tag major" contenteditable="false">
+                                    <div id="tag_0_1_text" class="tag_text">Differentiation</div>
+                                    <div id="tag_0_1_button" class="tag_button"></div>
+                                </div>
+                                <div id="tag_0_0" class="tag minor" contenteditable="false">
+                                    <div id="tag_0_1_text" class="tag_text">Product Rule</div>
+                                    <div id="tag_0_1_button" class="tag_button"></div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
