2151db3e8dc6b4700701a650e79d386f3b7a5192
benwhite10 <ben.white10@outlook.com>
2017-02-16 00:27:02 +0000

    Add cron jobs folder

---
+++ cron_jobs/backup.php
@@ -0,0 +1,40 @@
+<?php
+
+/* 
+ * The MIT License
+ *
+ * Copyright Error: on line 6, column 29 in Templates/Licenses/license-mit.txt
+The string doesn't match the expected date/time format. The string to parse was: "15-Feb-2017". The expected format was: "MMM d, yyyy". benwhite.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+$include_path = get_include_path();
+include_once $include_path . '/includes/db_functions.php';
+include_once $include_path . '/includes/session_functions.php';
+include_once $include_path . '/public_html/includes/mail_functions.php';
+include_once $include_path . '/public_html/classes/AllClasses.php';
+include_once $include_path . '/public_html/requests/core.php';
+include_once $include_path . '/public_html/libraries/PHPExcel.php';
+
+$return = db_back_up();
+$local = $return[0];
+$backup_name = $return[1];
+
+emailFile($local, $backup_name, "../../db_backups/$backup_name", $userid);
