5e822d61e1d737c6da3ccae5d5ea5a326c7dc72f
Ben <ben.white10@outlook.com>
2017-08-12 13:18:43 +0000

    Changes to add revision resource request, needs checking

--- public_html/requests/revision.php
+++ public_html/requests/revision.php
@@ -39,17 +39,17 @@ function download_worksheet() {
 	$text .= "\\end{questions} \\end{document}";
 
 	file_put_contents("test_latex.tex", $text);
-	exec('pdflatex -aux-directory="Temp/" -output-directory="output/" test_latex.tex');
+	//exec('pdflatex -aux-directory="Temp/" -output-directory="output/" test_latex.tex');
 	$dir = 'Temp';
 	$files = array_diff(scandir($dir), array('.','..')); 
 	foreach ($files as $file) { 
 		(is_dir("$dir/$file")) ? delTree("$dir/$file") : unlink("$dir/$file"); 
 	} 
 	rmdir($dir); 
-	unlink("test_latex.tex");
+	//unlink("test_latex.tex");
 
 	$response = array(
-		"success" => TRUE,
+		"success" => FALSE,
 		"url" => "requests/output/test_latex.pdf");
 	echo json_encode($response);
 	exit();
